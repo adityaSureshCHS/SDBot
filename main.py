f826b76895fb714faaca98c7389ce42ccf2d1556
Aditya Suresh <asuresh952@student.fuhsd.org>
2024-02-17 10:54:00 -0800

    minor change

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import pip._vendor.requests
 # `pip3 install assemblyai` (macOS)
 # `pip install assemblyai` (Windows)
-
 import assemblyai as aai
+
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return redirect("templates/main.html")
+
+@app.route("/record")
+def record():
+    return redirect("templates/record.html")
 
 aai.settings.api_key = "6353fb2f46144cfc9c2bc46924ba3689"
 transcriber = aai.Transcriber()
-
-transcript = transcriber.transcribe("https://storage.googleapis.com/aai-web-samples/news.mp4")
 # transcript = transcriber.transcribe("./my-local-audio-file.wav")
 
-print(transcript.text)
